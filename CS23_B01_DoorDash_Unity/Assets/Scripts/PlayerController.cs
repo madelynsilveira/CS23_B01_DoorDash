ac4b153e6f9049c35d86fdeaeb163509a3acfee8
Madelyn Silveira <136256203+mjsilveira@users.noreply.github.com>
2023-09-30 22:21:13 -0400

    Started rolling ball tutorial
    got to ball movement part of this tutorial https://learn.unity.com/tutorial/moving-the-player?uv=2022.3&projectId=5f158f1bedbc2a0020e51f0d#. The ball isn't moving and I don't know why.

---
+++ CS23_B01_DoorDash_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+    void Start()
+    {
+        rb = GetComponent <Rigidbody> ();
+    }
+
+    private void FixedUpdate() {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue) {
+        Debug.Log("OnMove called");
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+}
