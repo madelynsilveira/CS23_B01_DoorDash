4009993f9d04e417a902c6aeae26c867976c039e
Madelyn Silveira <136256203+mjsilveira@users.noreply.github.com>
2023-10-16 11:24:18 -0400

    car3 working as player character
    need to make movement better, add emission properties, and make wheels move seperately

--- DoorCrash/Assets/Scripts/PlayerController.cs
+++ DoorCrash/Assets/Scripts/PlayerController.cs
@@ -1,59 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private GameController gameController;
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
-    public float bounceForce = 50f;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 90f; // Degrees per second
 
-    public int deliveryCount;
-
-    public float speed = 0;
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.angularDrag = 40;
-        deliveryCount = 0;
+        // Get input from arrow keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (GameObject.FindWithTag ("GameController") != null) { gameController = GameObject.FindWithTag ("GameController").GetComponent<GameController>();
-         }
-    }
+        // Calculate movement direction
+        Vector3 moveDirection = new Vector3(0f, 0f, verticalInput).normalized;
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+        // Calculate rotation based on horizontal input
+        float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed * (Time.fixedDeltaTime + 1));
+        // Rotate the character
+        transform.Rotate(Vector3.up * rotation);
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("PickUp") && gameController.deliveries < 3){
-            other.gameObject.SetActive(false);
-            deliveryCount++;
-        }
-
-        if(other.gameObject.CompareTag("Trampoline")) {
-            Debug.Log("Hit a trampoline.");
-
-            // Calculate the bounce direction (assuming upward).
-            Vector3 bounceDirection = Vector3.up;
-
-            // Apply the bounce force to the object's Rigidbody.
-            rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
-        }
+        // Move the character forward or backward
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
+
+
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     private GameController gameController;
+//     private Rigidbody rb;
+//     private float movementX;
+//     private float movementY;
+//     public float bounceForce = 50f;
+
+//     public int deliveryCount;
+
+//     public float speed = 0;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//         deliveryCount = 0;
+
+//         if (GameObject.FindWithTag ("GameController") != null) { 
+//             gameController = GameObject.FindWithTag ("GameController").GetComponent<GameController>();
+//         }
+//     }
+
+//     void OnMove(InputValue movementValue)
+//     {
+//         Vector2 movementVector = movementValue.Get<Vector2>();
+//         movementX = movementVector.x;
+//         movementY = movementVector.y;
+//         Debug.Log("moving!" + this.gameObject.name);
+//     }
+
+//     void FixedUpdate()
+//     {
+//         Vector3 movement = new Vector3(movementX, movementY, movementY);
+//         rb.AddForce(movement * speed * (Time.fixedDeltaTime + 1));
+
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if(other.gameObject.CompareTag("PickUp") && gameController.deliveries < 3){
+//             other.gameObject.SetActive(false);
+//             deliveryCount++;
+//         }
+
+//         if(other.gameObject.CompareTag("Trampoline")) {
+//             Debug.Log("Hit a trampoline.");
+
+//             // Calculate the bounce direction (assuming upward).
+//             Vector3 bounceDirection = Vector3.up;
+
+//             // Apply the bounce force to the object's Rigidbody.
+//             rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+//         }
+//     }
+// }
